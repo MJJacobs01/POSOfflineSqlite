d3de2209cc7e3dbdab564eb87d8b4518eaddc0b7
MJJacobs01 <marthinusjacobs@gmail.com>
2024-01-01 09:59:05 +0200

    Top level build.gradle changed to build.gradle.kts
    Took 4 minutes

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         google()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:8.2.0'
+        classpath ("com.android.tools.build:gradle:8.2.0")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -16,10 +16,5 @@ allprojects {
     repositories {
         mavenCentral()
         google()
-        maven { url "https://jitpack.io" }
     }
-}
-
-tasks.register('clean', Delete) {
-    delete rootProject.buildDir
 }
