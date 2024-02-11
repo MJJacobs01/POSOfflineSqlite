e3c009d120812900afc915f2b6277d9103c6d949
MJJacobs01 <marthinusjacobs@gmail.com>
2024-02-11 12:37:18 +0200

    Migrated database to database module
    Took 42 seconds

--- app/src/main/java/com/lupawktu/possqlite/FragmentProduct.java
+++ app/src/main/java/com/lupawktu/possqlite/FragmentProduct.java
@@ -1,27 +1,27 @@
-package com.lupawktu.possqlite;
-
-import android.os.Bundle;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ListView;
-
-/**
- * Created by Mind on 6/3/2017.
- */
-public class FragmentProduct extends Fragment {
-    ListView listView;
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_product_n_category, container, false);
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    }
-}
+//package com.lupawktu.possqlite;
+//
+//import android.os.Bundle;
+//import androidx.annotation.Nullable;
+//import androidx.fragment.app.Fragment;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ListView;
+//
+///**
+// * Created by Mind on 6/3/2017.
+// */
+//public class FragmentProduct extends Fragment {
+//    ListView listView;
+//
+//    @Nullable
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        return inflater.inflate(R.layout.fragment_product_n_category, container, false);
+//    }
+//
+//    @Override
+//    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//    }
+//}
--- app/src/main/java/com/lupawktu/possqlite/MainMenu.java
+++ app/src/main/java/com/lupawktu/possqlite/MainMenu.java
@@ -1,68 +1,68 @@
-package com.lupawktu.possqlite;
-
-import android.content.Intent;
-import android.os.Bundle;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
-import android.widget.RelativeLayout;
-
-import com.lupawktu.possqlite.PublicMethod.SetDefaultStore;
-
-/**
- * Created by Mind on 6/2/2017.
- */
-public class MainMenu extends AppCompatActivity implements View.OnClickListener {
-
-    RelativeLayout product;
-
-    RelativeLayout order;
-
-    RelativeLayout setting;
-
-    RelativeLayout report;
-
-    RelativeLayout member;
-
-    RelativeLayout other;
-
-    SetDefaultStore setDefaultStore;
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
-        setDefaultStore = new SetDefaultStore(this);
-
-        getSupportActionBar().setTitle(setDefaultStore.getDefaultStore().get(SetDefaultStore.KEY_NAME));
-
-        product.setOnClickListener(this);
-        order.setOnClickListener(this);
-        setting.setOnClickListener(this);
-        report.setOnClickListener(this);
-        member.setOnClickListener(this);
-        other.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.product :
-                Intent a1 = new Intent(MainMenu.this, ProductCategory.class);
-                startActivity(a1);
-                break;
-            case R.id.order :
-                break;
-            case R.id.setting :
-                break;
-            case R.id.report :
-                break;
-            case R.id.member :
-                break;
-            case R.id.other :
-                Intent a6 = new Intent(MainMenu.this, DisplayStore.class);
-                startActivity(a6);
-                break;
-        }
-    }
-}
+//package com.lupawktu.possqlite;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import androidx.annotation.Nullable;
+//import androidx.appcompat.app.AppCompatActivity;
+//import android.view.View;
+//import android.widget.RelativeLayout;
+//
+//import com.lupawktu.possqlite.PublicMethod.SetDefaultStore;
+//
+///**
+// * Created by Mind on 6/2/2017.
+// */
+//public class MainMenu extends AppCompatActivity implements View.OnClickListener {
+//
+//    RelativeLayout product;
+//
+//    RelativeLayout order;
+//
+//    RelativeLayout setting;
+//
+//    RelativeLayout report;
+//
+//    RelativeLayout member;
+//
+//    RelativeLayout other;
+//
+//    SetDefaultStore setDefaultStore;
+//
+//    @Override
+//    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main_menu);
+//        setDefaultStore = new SetDefaultStore(this);
+//
+//        getSupportActionBar().setTitle(setDefaultStore.getDefaultStore().get(SetDefaultStore.KEY_NAME));
+//
+//        product.setOnClickListener(this);
+//        order.setOnClickListener(this);
+//        setting.setOnClickListener(this);
+//        report.setOnClickListener(this);
+//        member.setOnClickListener(this);
+//        other.setOnClickListener(this);
+//    }
+//
+//    @Override
+//    public void onClick(View view) {
+//        switch (view.getId()){
+//            case R.id.product :
+//                Intent a1 = new Intent(MainMenu.this, ProductCategory.class);
+//                startActivity(a1);
+//                break;
+//            case R.id.order :
+//                break;
+//            case R.id.setting :
+//                break;
+//            case R.id.report :
+//                break;
+//            case R.id.member :
+//                break;
+//            case R.id.other :
+//                Intent a6 = new Intent(MainMenu.this, DisplayStore.class);
+//                startActivity(a6);
+//                break;
+//        }
+//    }
+//}
